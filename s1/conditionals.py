69c2192d25e3cc7449e7b775c75259ac31feed6a
neenjaw <tim@neenjaw.com>
2018-02-02 19:39:15 -0700

    update

---
+++ s1/conditionals.py
@@ -0,0 +1,10 @@
+def minutes_to_hours(minutes):
+    hours = minutes / 60
+    return hours
+
+def seconds_to_hours(seconds):
+    hours = seconds / 3600
+    return hours
+
+print(minutes_to_hours(40))
+
