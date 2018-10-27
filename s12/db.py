356b6d0326e72da8df35f06f6495c407c07da64e
Tim Austin <tim@neenjaw.com>
2018-10-27 03:06:52 -0600

    db crud done, now app5 started

--- s12/db.py
+++ s12/db.py
@@ -12,11 +12,11 @@ def close_db_conn(db_conn):
 def create_table(db_conn):
     cursor = db_conn.cursor()
 
-    # cursor.execute("DROP TABLE IF EXISTS fruit;")
+    cursor.execute("DROP TABLE IF EXISTS fruit;")
 
     create_table_sql = """
         CREATE TABLE IF NOT EXISTS fruit (
-            item TEXT PRIMARY KEY,
+            name TEXT PRIMARY KEY,
             quantity INTEGER, 
             price REAL
         );
@@ -25,16 +25,16 @@ def create_table(db_conn):
 
     db_conn.commit()
 
-def insert(db_conn, item, quantity, price):
+def insert(db_conn, name, quantity, price):
     cursor = db_conn.cursor()
 
     # this is not safe, injection vulnerable
-    # cursor.execute(f"INSERT INTO fruits (item, quantity, price) VALUES ({item}, {quantity}, {price})")
+    # cursor.execute(f"INSERT INTO fruits (name, quantity, price) VALUES ({name}, {quantity}, {price})")
 
-    cursor.execute("INSERT INTO fruit (item, quantity, price) VALUES (?,?,?)", (item, quantity, price))
+    cursor.execute("INSERT INTO fruit (name, quantity, price) VALUES (?,?,?)", (name, quantity, price))
     db_conn.commit()
 
-def view_rows(db_conn):
+def view(db_conn):
     cursor = db_conn.cursor()
 
     view_sql = """
@@ -43,17 +43,45 @@ def view_rows(db_conn):
     cursor.execute(view_sql)
     rows = cursor.fetchall()
     return rows
+    
+def view_formatted(db_conn):
+    formatted_result = [f"{name:<20}{quantity:<6}{price:>5}" for name, quantity, price in view(db_conn)]
+    name, quantity, price = "Name", "Qty", "Price"
+    return "\n".join([f"{name:<20}{quantity:<6}{price:>5}"] + formatted_result)
+
+def delete(db_conn, name):
+    cursor = db_conn.cursor()
+
+    delete_sql = """
+        DELETE FROM fruit WHERE name=?
+        """
+    cursor.execute(delete_sql, (name,))
+    db_conn.commit()
+
+def update_price(db_conn, name, price):
+    cursor = db_conn.cursor()
+
+    update_sql = """
+        UPDATE fruit SET price=? WHERE name=?
+        """    
+    cursor.execute(update_sql, (price, name))
+    db_conn.commit()
+
+
 
 
 conn = get_db_conn()
+
 create_table(conn)
-insert(conn, "coffee cup", 10, 5)
 
-for row in view_rows(conn):
-    print(row)
+insert(conn, "coffee", 10, 5)
 
-formatted_result = [f"{name:<20}{quantity:<6}{price:>5}" for name, quantity, price in view_rows(conn)]
-name, quantity, price = "Name", "Qty", "Price"
-print("\n".join([f"{name:<20}{quantity:<6}{price:>5}"] + formatted_result))
+update_price(conn, "coffee", 8.56)
+
+print(view_formatted(conn))
+
+delete(conn, "coffee")
+
+print(view_formatted(conn))
 
 conn.close()
